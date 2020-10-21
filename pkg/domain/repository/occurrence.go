18e2d4a4fd25042376397095d20c12ea122e802c
A Ruiz <luis.alonso.16@hotmail.com>
2020-10-20 20:57:19 -0600

    Update occurrence repository

--- pkg/domain/repository/occurrence.go
+++ pkg/domain/repository/occurrence.go
@@ -6,8 +6,17 @@ import (
 	"github.com/maestre3d/lifetrack-sanbox/pkg/domain/aggregate"
 )
 
+// OccurrenceCriteria sets the Occurrence fetching strategy
+type OccurrenceCriteria struct {
+	ID       string
+	Activity string
+	Limit    int64
+	Token    string
+}
+
 // Occurrence handles aggregate.Occurrence persistence
 type Occurrence interface {
 	Save(ctx context.Context, occurrence aggregate.Occurrence) error
-	Fetch(ctx context.Context, id string) (*aggregate.Occurrence, error)
+	Fetch(ctx context.Context, criteria OccurrenceCriteria) ([]*aggregate.Occurrence, string, error)
+	Remove(ctx context.Context, id string) error
 }
