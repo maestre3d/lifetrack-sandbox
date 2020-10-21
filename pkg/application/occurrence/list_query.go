697d5a9adf31e69d9c34d9e490a4f94f4c5ae400
A Ruiz <luis.alonso.16@hotmail.com>
2020-10-20 20:57:57 -0600

    Add list query occurrence

---
+++ pkg/application/occurrence/list_query.go
@@ -0,0 +1,34 @@
+package occurrence
+
+import (
+	"context"
+
+	"github.com/maestre3d/lifetrack-sanbox/pkg/domain/adapter"
+	"github.com/maestre3d/lifetrack-sanbox/pkg/domain/model"
+	"github.com/maestre3d/lifetrack-sanbox/pkg/domain/repository"
+)
+
+type ListQuery struct {
+	repo repository.Occurrence
+}
+
+func NewListQuery(r repository.Occurrence) *ListQuery {
+	return &ListQuery{repo: r}
+}
+
+// Filter sets the Occurrence fetching strategy
+type Filter struct {
+	Activity string `json:"activity"`
+	Limit    int64  `json:"limit"`
+	Token    string `json:"token"`
+}
+
+func (q ListQuery) Query(ctx context.Context, filter Filter) ([]*model.Occurrence, string, error) {
+	ocs, nextPage, err := q.repo.Fetch(ctx, repository.OccurrenceCriteria{
+		Activity: filter.Activity,
+		Limit:    filter.Limit,
+		Token:    filter.Token,
+	})
+
+	return adapter.BulkUnmarshalPrimitiveOccurrence(ocs), nextPage, err
+}
