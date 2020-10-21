6bdb03997da8a0f4742d285469f2512d76990f83
A Ruiz <luis.alonso.16@hotmail.com>
2020-10-20 20:59:08 -0600

    Update http sandbox

--- cmd/http-sandbox/main.go
+++ cmd/http-sandbox/main.go
@@ -5,56 +5,80 @@ import (
 	"log"
 	"time"
 
+	"github.com/maestre3d/lifetrack-sanbox/pkg/infrastructure/persistence/poccurrence"
+
 	"github.com/alexandria-oss/common-go/exception"
 	"github.com/google/uuid"
 	"github.com/maestre3d/lifetrack-sanbox/pkg/application/occurrence"
-	"github.com/maestre3d/lifetrack-sanbox/pkg/infrastructure/persistence"
 )
 
 func main() {
-	repo := persistence.NewOccurrenceInMemory()
+	repo := poccurrence.NewOccurrenceInMemory()
 	handler := occurrence.NewCreateCommandHandler(repo)
 
+	act1 := uuid.New().String()
 	id, err := handler.Invoke(occurrence.CreateCommand{
 		Ctx:        context.Background(),
-		ActivityID: uuid.New().String(),
+		ActivityID: act1,
 		StartTime:  time.Now().Unix(),
 		EndTime:    time.Now().Add(time.Minute * 32).Unix(),
 	})
 	if err != nil {
 		log.Fatal(exception.GetDescription(err))
 	}
-
 	log.Printf("category created: %s", id)
 
-	query := occurrence.NewGetQuery(repo)
-	oc, err := query.Query(context.Background(), id)
-	if err != nil {
-		log.Fatal(exception.GetDescription(err))
-	}
-
-	log.Printf("%+v", oc)
-
-	time.Sleep(time.Second * 3)
-
-	updateHandler := occurrence.NewUpdateCommandHandler(repo)
-	err = updateHandler.Invoke(occurrence.UpdateCommand{
+	id2, err := handler.Invoke(occurrence.CreateCommand{
 		Ctx:        context.Background(),
-		ID:         id,
-		StartTime:  time.Now().Add(time.Hour * 1).Unix(),
-		EndTime:    time.Now().Add(time.Minute * 88).Unix(),
 		ActivityID: uuid.New().String(),
+		StartTime:  time.Now().Add(time.Minute * 15).Unix(),
+		EndTime:    time.Now().Add(time.Minute * 48).Unix(),
 	})
 	if err != nil {
 		log.Fatal(exception.GetDescription(err))
 	}
+	log.Printf("category created: %s", id2)
 
-	oc2, err := query.Query(context.Background(), id)
-	if err != nil {
-		log.Fatal(exception.GetDescription(err))
+	list := occurrence.NewListQuery(repo)
+	ocs, _, err := list.Query(context.Background(), occurrence.Filter{
+		Activity: act1,
+		Limit:    10,
+		Token:    "",
+	})
+
+	for _, oc := range ocs {
+		log.Print(oc)
 	}
 
-	log.Printf("%+v", oc2)
+	/*
+		query := occurrence.NewGetQuery(repo)
+		oc, err := query.Query(context.Background(), id)
+		if err != nil {
+			log.Fatal(exception.GetDescription(err))
+		}
+		log.Printf("%+v", oc)
+
+		time.Sleep(time.Second * 3)
+
+		updateHandler := occurrence.NewUpdateCommandHandler(repo)
+		err = updateHandler.Invoke(occurrence.UpdateCommand{
+			Ctx:        context.Background(),
+			ID:         id,
+			StartTime:  time.Now().Add(time.Hour * 1).Unix(),
+			EndTime:    time.Now().Add(time.Minute * 88).Unix(),
+			ActivityID: uuid.New().String(),
+		})
+		if err != nil {
+			log.Fatal(exception.GetDescription(err))
+		}
+
+		rmCmd := occurrence.NewRemoveCommandHandler(repo)
+		err = rmCmd.Invoke(occurrence.RemoveCommand{
+			Ctx: context.Background(),
+			ID:  id,
+		})
+		log.Printf("removed %s occurrence", id)
+	*/
 
 	/*
 		oc, err := aggregate.NewOccurrence(uuid.New().String(), time.Now(), time.Now().Add(time.Minute*10))
