d313894eb911f0c184ee5bba8bcff0c8710f495a
Alonso R <luis.alonso.16@hotmail.com>
2020-10-24 17:40:27 -0600

    Refactor activity exceptions

--- pkg/domain/exceptions/activity.go
+++ pkg/domain/exceptions/activity.go
@@ -4,7 +4,7 @@ import "github.com/alexandria-oss/common-go/exception"
 
 var (
 	// ErrEmptyCategoryID category field is empty
-	ErrEmptyCategoryID = exception.NewRequiredField("category")
+	ErrEmptyCategoryID = exception.NewRequiredField("category_id")
 	// ErrBelowTitleLength title field is below 2 characters
 	ErrBelowTitleLength = exception.NewFieldRange("title", "2", "512")
 	// ErrActivityMarshaling problem occurred while activity marshaling
@@ -12,6 +12,6 @@ var (
 	// ErrActivityNotFound activity was not found
 	ErrActivityNotFound = exception.NewNotFound("activity")
 	// ErrInvalidActivityFilter activity fetch filter has no valid values
-	ErrInvalidActivityFilter = exception.NewFieldFormat("activity_filter", "activity__id, "+
+	ErrInvalidActivityFilter = exception.NewFieldFormat("activity_filter", "activity_id, "+
 		"category_id, title, page_size, page_token")
 )
