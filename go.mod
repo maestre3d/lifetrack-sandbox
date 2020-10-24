a580ecc4e8307bc3b91dea57f48c60ccf66f7e88
Alonso R <luis.alonso.16@hotmail.com>
2020-10-24 01:45:50 -0600

    Add in memory event bus implementation

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/alexandria-oss/common-go v0.3.0-alpha
+	github.com/asaskevich/EventBus v0.0.0-20200907212545-49d423059eef
 	github.com/google/uuid v1.1.2
 )
---
+++ pkg/infrastructure/eventbus/in_memory.go
@@ -0,0 +1,70 @@
+package eventbus
+
+import (
+	"context"
+	"sync"
+
+	"github.com/maestre3d/lifetrack-sanbox/pkg/domain/model"
+
+	"github.com/asaskevich/EventBus"
+	"github.com/maestre3d/lifetrack-sanbox/pkg/domain/event"
+)
+
+var (
+	inMemorySingleton *InMemory
+	inMemoryLock      = new(sync.Once)
+)
+
+// InMemory event.Bus in memory concrete implementation
+type InMemory struct {
+	bus EventBus.Bus
+	cfg model.Configuration
+	mu  *sync.Mutex
+}
+
+// NewInMemory creates a new InMemory bus
+func NewInMemory(c model.Configuration) *InMemory {
+	inMemoryLock.Do(func() {
+		inMemorySingleton = &InMemory{bus: EventBus.New(), cfg: c, mu: new(sync.Mutex)}
+	})
+	return inMemorySingleton
+}
+
+// Publish sends a group of Domain events to all subscribers
+func (b *InMemory) Publish(_ context.Context, events ...event.Domain) error {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	for _, e := range events {
+		e.Version = b.cfg.Version
+		e.Stage = b.cfg.Stage
+
+		eJSON, err := e.MarshalBinary()
+		if err != nil {
+			return err
+		}
+		b.bus.Publish(e.Topic, eJSON)
+	}
+	return nil
+}
+
+// SubscribeTo adds a new subscription to an specific topic, returns a channel of Domain if exists
+func (b *InMemory) SubscribeTo(_ context.Context, topic string) (chan event.Domain, error) {
+	eventStream := make(chan event.Domain)
+	err := make(chan error)
+	go func() {
+		if errS := b.bus.SubscribeAsync(topic, func(incomeEvent []byte) {
+			e := new(event.Domain)
+			if errM := e.UnmarshalBinary(incomeEvent); errM != nil {
+				err <- errM
+				return
+			}
+
+			eventStream <- *e
+		}, false); err != nil {
+			err <- errS
+		}
+	}()
+
+	return eventStream, <-err
+}
