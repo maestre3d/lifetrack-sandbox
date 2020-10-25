1cf24380f34d1c0066635794c9e002b14f2de913
Alonso R <luis.alonso.16@hotmail.com>
2020-10-24 22:32:47 -0600

    Omit category empty values on primitive model

--- pkg/domain/model/category.go
+++ pkg/domain/model/category.go
@@ -6,7 +6,7 @@ type Category struct {
 	UserID      string `json:"user_id"`
 	Name        string `json:"name"`
 	Description string `json:"description,omitempty"`
-	TargetTime  int64  `json:"target_time"`
+	TargetTime  int64  `json:"target_time,omitempty"`
 	Picture     string `json:"picture,omitempty"`
 	CreateTime  int64  `json:"create_time"`
 	UpdateTime  int64  `json:"update_time"`
